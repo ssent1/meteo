0e749586a603d1404e7ea5df7e0e21099cce2604
Syd Salmon <syd@proganica.com>
2020-08-03 20:55:27 -0400

    Fixed layout. Airport code fits correctly.

--- meteo.sh
+++ meteo.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 DateColumn=28
-TimeColumn=16
-curl http://wttr.in/YKF?0F --silent --max-time 3 > /tmp/now-weather
+TimeColumn=17
+curl https://wttr.in/YKF?0F --silent --max-time 3 > /tmp/now-weather
 readarray aWeather < /tmp/now-weather
 rm -f /tmp/now-weather
-if [[ "${aWeather[0]}" == "Weather report:"* ]] ; then
+if [[ ${aWeather[0]} == "Weather report:"* ]]; then
     WeatherSuccess=true
-    echo "${aWeather[@]}"
+    echo " ${aWeather[@]}"
 else
     WeatherSuccess=false
     echo "+=====================+"
@@ -18,30 +18,30 @@ else
     echo "+=====================+"
     echo " "
 fi
-    echo " "
-#--------- DATE -------------------------------------------------------------
+echo " "
+#--------- CALENDAR -------------------------------------------------------------
 tput sc
 i=0
 while [ $((++i)) -lt 10 ]; do tput cuu1; done
-if [[ "$WeatherSuccess" == true ]] ; then
-    Column=$((DateColumn - 10))
+if [[ $WeatherSuccess == true ]]; then
+    Column=$((DateColumn - 8))
     tput cuf $Column
     printf "          "
 else
     tput cuf $DateColumn
 fi
 cal > /tmp/terminal1
-tr -cd '\11\12\15\40\60-\136\140-\176' < /tmp/terminal1  > /tmp/terminal
+tr -cd '\11\12\15\40\60-\136\140-\176' < /tmp/terminal1 > /tmp/terminal
 CalLineCnt=1
 Today=$(date +"%e")
 printf "\033[34m"
 while IFS= read -r Cal; do
     printf "%s" "$Cal"
-    if [[ $CalLineCnt -gt 2 ]] ; then
+    if [[ $CalLineCnt -gt 2 ]]; then
         tput cub 22
-        for (( j=0 ; j <= 18 ; j += 3 )) ; do
-            Test=${Cal:$j:2}
-            if [[ "$Test" == "$Today" ]] ; then
+        for ((j = 0; j <= 18; j += 3)); do
+            Test=${Cal:j:2}
+            if [[ $Test == "$Today" ]]; then
                 printf "\033[0;32m\033[7m"
                 printf "%s" "$Today"
                 printf "\033[0m"
